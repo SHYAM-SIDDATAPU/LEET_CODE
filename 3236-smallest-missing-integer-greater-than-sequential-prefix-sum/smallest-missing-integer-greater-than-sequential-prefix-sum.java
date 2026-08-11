ea323722de043b5880a3b2db18be84669444dd1a
shyam siddantapu <siddatapushyam@gmail.com>
2026-08-11 21:29:39 +0530

    Time: 1 ms (93.72%) | Memory: 44.2 MB (50.35%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int x : nums) {
+            set.add(x);
+        }
+
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1] + 1) {
+                break;
+            }
+
+            sum += nums[i];
+        }
+
+        while (set.contains(sum)) {
+            sum++;
+        }
+
+        return sum;
+    }
+}
