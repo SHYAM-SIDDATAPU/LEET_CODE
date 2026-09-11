88575d14af87ad86f2de06aa174670dd26141d86
shyam siddantapu <siddatapushyam@gmail.com>
2026-09-11 19:49:49 +0530

    Time: 1 ms (56.42%) | Memory: 45.5 MB (62.91%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -14,26 +14,18 @@
  * }
  */
 class Solution {
+    int c=0;
     public int averageOfSubtree(TreeNode root) {
-        if(root==null) return 0;
-        Queue<TreeNode> q= new LinkedList<>();
-        q.offer(root);
-        int c=0;
-        while(!q.isEmpty()){
-            TreeNode t=q.poll();
-            if(t.val==sum(t,0)/count(t,0)) c++;
-            System.out.println(count(t,0));
-            if(t.left!=null) q.offer(t.left);
-            if(t.right!=null) q.offer(t.right);
-        }
+        avgc(root);
         return c;
     }
-    int sum(TreeNode t,int s){
-        if(t==null) return 0;
-        return t.val+sum(t.left,s)+sum(t.right,s);
-    }
-    int count(TreeNode t,int s){
-        if(t==null) return 0;
-        return 1+count(t.left,s)+count(t.right,s);
+    int[] avgc(TreeNode root){
+        if(root==null) return new int []{0,0};
+        int [] l= avgc(root.left);
+        int [] r= avgc(root.right);
+        int s=root.val+l[0]+r[0];
+        int c1=1+l[1]+r[1];
+        if(s/c1==root.val) c++;
+        return new int[]{s,c1};
     }
 }
